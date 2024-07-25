a4f6df5534ab943adb29422c610f21a72cecea26
joshuajrodrigues <rodrigues.joshr@gmail.com>
2024-07-25 18:51:20 +0530

    example basic setup

---
+++ editor/src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Editor from './components/editor'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className=' text-xl m-5 underline'>
+        Totoro editor example
+      </h1>
+      <Editor />
+    </>
+  )
+}
+
+export default App
